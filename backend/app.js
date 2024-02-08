ba3c5675d30aa87b495c07fdd09931dd768176af
Vedant Domadiya <vedantdomadiya1809@gmail.com>
2024-02-08 18:35:01 +0530

    Part 3 completed

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require('cors');
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+// import routes
+const authRoutes = require('./routes/authRoutes');
+
 
 // Database connection
 mongoose.connect(process.env.DATABASE, {
@@ -12,6 +18,27 @@ mongoose.connect(process.env.DATABASE, {
   })
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
+
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+// error middleware
+app.use(errorHandler);
+
+
+//ROUTES MIDDLEWARE
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+app.use('/api', authRoutes);
 
 
 //port
